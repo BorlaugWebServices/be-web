5f0c834bcaf1805a1c1f9c0e70f0fc77c37c62fb
Bopsi <bshangout@gmail.com>
2020-07-15 15:18:18 +0530

    modification to id page

--- src/components/objects/Identity/Identity.vue
+++ src/components/objects/Identity/Identity.vue
@@ -97,65 +97,124 @@
                                 </tr>
                                 <tr v-for="prop in identity.properties">
                                     <td class="p-2">{{prop.name}}</td>
-                                    <td class="p-2"> {{prop.fact}}</td>
+                                    <td class="p-2">{{prop.fact}}</td>
                                 </tr>
                             </table>
                         </dd>
                     </div>
                 </dl>
+                <hr/>
+                <dl class="row mb-0" v-if="identity">
+                    <div class="col-sm-2 text-sm-right">
+                        <dt>Claims</dt>
+                    </div>
+                    <div class="col-sm-9 text-sm-left" v-if="identity.claims.length>0">
+                        <div class="card border mb-2">
+                            <div class="card-body p-2" >
+                                <dt>Description :</dt>
+                                <dd>{{identity.claims[claimIndex].description}}</dd>
+
+                                <dt>Statments :</dt>
+                                <dd>
+                                    <table class="table table-bordered">
+                                        <tr>
+                                            <th class="p-2 font-bold">Name</th>
+                                            <th class="p-2 font-bold">Fact</th>
+                                        </tr>
+                                        <tr v-for="st    in identity.claims[claimIndex].statements">
+                                            <td class="p-2">{{st.name}}</td>
+                                            <td class="p-2">{{st.fact}}</td>
+                                        </tr>
+                                    </table>
+                                </dd>
+
+                                <dt class="m-b-5">Created By :</dt>
+                                <dd class="mb-3">
+                                    <router-link :to="{name: 'identity', params : { did: $options.filters.did(identity.claims[claimIndex].created_by.id) }}">
+                                        <Blockie :address="identity.claims[claimIndex].created_by.id"/>
+                                        {{identity.claims[claimIndex].created_by.id | did }}
+                                    </router-link>
+                                </dd>
+
+                                <dt class="m-b-5">Attested By :</dt>
+                                <dd class="mb-3">
+                                    <router-link :to="{name: 'identity', params : { did: $options.filters.did(identity.claims[claimIndex].attestation.attested_by.id) }}">
+                                        <Blockie :address="identity.claims[claimIndex].attestation.attested_by.id" class=""/>
+                                        {{identity.claims[claimIndex].attestation.attested_by.id | did }}
+                                    </router-link>
+                                </dd>
+
+                                <dt class="m-t-20 m-b-5">Valid Until :</dt>
+                                <dd>
+                                    {{identity.claims[claimIndex].attestation.valid_until | date}}
+                                </dd>
+                            </div>
+                        </div>
+
+                        <div class="w-25 float-left">
+                            <button class="btn btn-orange text-white float-left " @click="prev" :disabled="claimIndex === 0">Previous</button>
+                        </div>
+                        <div class="w-50 float-left text-center pt-2">
+                            <p>Showing <strong>#{{claimIndex+1}}</strong> of <strong>{{identity.claims.length}}</strong> claims</p>
+                        </div>
+                        <div class="w-25 float-left">
+                            <button class="btn btn-orange text-white float-right" @click="next" :disabled="claimIndex === (identity.claims.length-1)">Next</button>
+                        </div>
+                    </div>
+                </dl>
             </div>
         </div>
 
-        <div class="card">
-            <div class="card-header row m-b-0 p-b-0">
-                <div class="card-header-title">
-                    <h4>Identity Activities</h4>
-                </div>
-                <div class="card-header-icon">
-                    <h3><i class="fas fa-id-card card-title text-orange"/></h3>
-                </div>
-            </div>
+<!--        <div class="card">-->
+<!--            <div class="card-header row m-b-0 p-b-0">-->
+<!--                <div class="card-header-title">-->
+<!--                    <h4>Identity Activities</h4>-->
+<!--                </div>-->
+<!--                <div class="card-header-icon">-->
+<!--                    <h3><i class="fas fa-id-card card-title text-orange"/></h3>-->
+<!--                </div>-->
+<!--            </div>-->
 
-            <div class="card-body mg-b-20 p-t-0">
-                <div class="table-responsive blocks" v-if="activities.length > 0">
-                    <table class="table v-middle">
-                        <thead>
-                        <tr>
-                            <th class="border-top-0 font-weight-bold">#</th>
-                            <th class="border-top-0 font-weight-bold">Activity</th>
-                            <th class="border-top-0 font-weight-bold">Transaction Hash</th>
-                            <th class="border-top-0 font-weight-bold">Status</th>
-                            <th class="border-top-0 font-weight-bold">Timestamp</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr v-for="(activity,i) in activities">
-                            <td>{{i+1 }}</td>
-                            <td>{{activity.method.method}}</td>
-                            <td>
-                                <router-link :to="{ name: 'transaction', params: { hash: activity.hash }}"
-                                             :title="activity.hash">
-                                    {{activity.hash | truncate(32, '')}}
-                                </router-link>
-                            </td>
-                            <td>
-                                <span class="badge badge-pill badge-success font-bold" v-if="activity.isSuccess">
-                                    <i class="fa fa-check-circle"/> SUCCESS
-                                </span>
-                                <span class="badge badge-pill badge-danger font-bold" v-else>
-                                    <i class="fas fa-exclamation-circle"></i> FAILED
-                                </span>
-                            </td>
-                            <td>{{activity.timestamp.toString() | timestamp}}</td>
-                        </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="p-b-10" v-else>
-                    <h4 class="text-muted text-center">No Activities found</h4>
-                </div>
-            </div>
-        </div>
+<!--            <div class="card-body mg-b-20 p-t-0">-->
+<!--                <div class="table-responsive blocks" v-if="activities.length > 0">-->
+<!--                    <table class="table v-middle">-->
+<!--                        <thead>-->
+<!--                        <tr>-->
+<!--                            <th class="border-top-0 font-weight-bold">#</th>-->
+<!--                            <th class="border-top-0 font-weight-bold">Activity</th>-->
+<!--                            <th class="border-top-0 font-weight-bold">Transaction Hash</th>-->
+<!--                            <th class="border-top-0 font-weight-bold">Status</th>-->
+<!--                            <th class="border-top-0 font-weight-bold">Timestamp</th>-->
+<!--                        </tr>-->
+<!--                        </thead>-->
+<!--                        <tbody>-->
+<!--                        <tr v-for="(activity,i) in activities">-->
+<!--                            <td>{{i+1 }}</td>-->
+<!--                            <td>{{activity.method.method}}</td>-->
+<!--                            <td>-->
+<!--                                <router-link :to="{ name: 'transaction', params: { hash: activity.hash }}"-->
+<!--                                             :title="activity.hash">-->
+<!--                                    {{activity.hash | truncate(32, '')}}-->
+<!--                                </router-link>-->
+<!--                            </td>-->
+<!--                            <td>-->
+<!--                                <span class="badge badge-pill badge-success font-bold" v-if="activity.isSuccess">-->
+<!--                                    <i class="fa fa-check-circle"/> SUCCESS-->
+<!--                                </span>-->
+<!--                                <span class="badge badge-pill badge-danger font-bold" v-else>-->
+<!--                                    <i class="fas fa-exclamation-circle"></i> FAILED-->
+<!--                                </span>-->
+<!--                            </td>-->
+<!--                            <td>{{activity.timestamp.toString() | timestamp}}</td>-->
+<!--                        </tr>-->
+<!--                        </tbody>-->
+<!--                    </table>-->
+<!--                </div>-->
+<!--                <div class="p-b-10" v-else>-->
+<!--                    <h4 class="text-muted text-center">No Activities found</h4>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </div>-->
     </div>
     <div class="row" v-else>
         <div class="col-12">
@@ -188,7 +247,8 @@
                 identity: null,
                 activities: [],
                 show: false,
-                flag: 'SEARCHING'
+                flag: 'SEARCHING',
+                claimIndex: 0
             }
         },
         mounted() {
@@ -198,7 +258,7 @@
         methods: {
             async init() {
                 await this.getIdentity();
-                await this.getIdentityActivities();
+                // await this.getIdentityActivities();
             },
             async getIdentity() {
                 if(this.did !== null) {
@@ -235,6 +295,16 @@
                 let d       = new Date(0);
                 d.setSeconds(seconds);
                 return d;
+            },
+            prev() {
+                if(this.identity && this.claimIndex > 0) {
+                    this.claimIndex--;
+                }
+            },
+            next() {
+                if(this.identity) {
+                    this.claimIndex++;
+                }
             }
         }
     }
